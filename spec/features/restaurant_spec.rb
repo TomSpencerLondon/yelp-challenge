4c5e2a50c4c352f74e2952d175f18791acee4f22
RMCollins175 <rorycollins@me.com>
2017-11-14 15:20:11 +0000

    amended creating restaurant feature test to include filling form

--- spec/features/restaurant_spec.rb
+++ spec/features/restaurant_spec.rb
@@ -3,6 +3,9 @@ require 'rails_helper'
 feature 'restuarant' do
   scenario 'can create a restaurant' do
     visit('/restaurants/new')
-    expect(page).to have_button('Create Restaurant')
+    fill_in :restaurant_name, with: 'KFC'
+    fill_in :restaurant_description, with: 'Finger-licking good'
+    click_button 'Create Restaurant'
+    expect(page).to have_content(' Name: KFC Description: Finger-licking good')
   end
 end
