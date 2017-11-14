2a05ac8976f5c9c20c8ae103a5d1f8121320ca28
ainsleybc <ab.chang@me.com>
2017-11-14 17:35:11 +0000

    fixed bug with random merge code

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,10 +1,7 @@
 class Restaurant < ApplicationRecord
-<<<<<<< HEAD
 
   validates :name, presence: true, length: { minimum: 3 }
   validates :description, presence: true, length: { minimum: 5 }                    
 
-=======
   has_many :reviews, dependent: :destroy
->>>>>>> 350e28ea6a0281586924071faa9645fa5bed1679
 end
