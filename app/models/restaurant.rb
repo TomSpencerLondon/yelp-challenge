e2dfac66e975dc938e64072a4fd871c185a839f9
ainsleybc <ab.chang@me.com>
2017-11-14 16:03:32 +0000

    fixed some pending restaurant tests, added validations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,6 @@
 class Restaurant < ApplicationRecord
+
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :description, presence: true, length: { minimum: 5 }                    
+
 end
--- spec/controllers/restaurants_controller_spec.rb
+++ spec/controllers/restaurants_controller_spec.rb
@@ -29,12 +29,12 @@ RSpec.describe RestaurantsController, type: :controller do
   # Restaurant. As you add validations to Restaurant, be sure to
   # adjust the attributes here as well.
   let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
+    { name: 'test name', description: 'test description for restaurant' }
   }
 
   let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
-  }
+    { name: '', description: '' }    
+}
 
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
