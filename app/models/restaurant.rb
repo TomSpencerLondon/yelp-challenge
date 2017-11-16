6e89c696c4a036b90b8d9b16a6f9a9298a1e10af
hugosykes <hugo@denmeadfarm.co.uk>
2017-11-16 01:12:45 +0000

    Attempted to add relations between users and both other models. One commented out line

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,7 +1,6 @@
 class Restaurant < ApplicationRecord
-
+  # belongs_to :user
   validates :name, presence: true, length: { minimum: 3 }
   validates :description, presence: true, length: { minimum: 5 }                    
-
   has_many :reviews, dependent: :destroy
 end
